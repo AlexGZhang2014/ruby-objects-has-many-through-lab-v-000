67b3df6877f2a9b5eb60c5d0bcf9cd3e27fe4193
Alex Zhang <alexgzhang92@gmail.com>
2018-05-31 12:27:11 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,17 @@
 class Doctor
   attr_accessor :name
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new()
 end
